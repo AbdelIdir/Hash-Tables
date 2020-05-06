4976b85aaaca4a6441b609489a0c527fa1db46a3
AbdelI <homeworkoracle@protonmail.com>
2020-05-06 16:30:09 +0200

    hash index function

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -27,6 +27,7 @@ class HashTable:
 
         Implement this, and/or DJB2.
         """
+
     def hash_djb2(self, key):
         hash = 5381
         for x in key:
@@ -38,8 +39,7 @@ class HashTable:
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        # return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.hash_djb2(key) % self.capacity
 
     def put(self, key, value):
         """
